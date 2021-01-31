b18b10583d813a50e01806050ca2c3a1c177006d
Michael Vorburger <mike@vorburger.ch>
2021-01-31 04:20:58 +0100

    add new kube-hello.sh

---
+++ archlinux/kubernetes/kube-hello.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+set -euox pipefail
+
+# https://github.com/GoogleCloudPlatform/kubernetes-engine-samples/blob/master/hello-app/main.go
+kubectl create deployment hello --image=gcr.io/google-samples/hello-app:1.0
+kubectl expose deployment hello --type=NodePort --port=1234 --target-port=8080
+
+kubectl describe pod hello
+kubectl describe deployment hello
+kubectl get deployment hello
+
+kubectl wait --for=condition=available deployment/hello
+
+curl http://$(kubectl get nodes -o=jsonpath={.items[0].status.addresses[0].address}):$(kubectl get svc hello --output=jsonpath='{range .spec.ports[0]}{.nodePort}')
+
+#kubectl delete service hello
+#kubectl delete deployment hello
