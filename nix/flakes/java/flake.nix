48f37e6a2dca9deb84358952229062113d6c741e
Michael Vorburger <mike@vorburger.ch>
2025-06-28 23:28:19 +0900

    nix: shellHook exec fish

--- nix/flakes/java/flake.nix
+++ nix/flakes/java/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
 
   outputs = { self, nixpkgs }:
     let
@@ -11,9 +11,13 @@
         default = pkgs.mkShell {
           packages = with pkgs; [
             protobuf
+            # FYI Fish v3.7.1 on 24.11 but 4.0.2 on 25.05:
+            fish
           ];
           shellHook = ''
             protoc --version
+            # TODO How to use users preferred $SHELL in nix develop, instead of hard-code my fish preference?
+            exec fish
           '';
         };
       });
