199d1b4b3c635cefde36fb74480a684e2587f101
Michael Vorburger <mike@vorburger.ch>
2025-06-29 00:22:04 +0900

    nix: Clean-up fish related stuff

--- nix/flakes/java/flake.nix
+++ nix/flakes/java/flake.nix
@@ -11,15 +11,9 @@
         default = pkgs.mkShell {
           packages = with pkgs; [
             protobuf
-            # FYI Fish v3.7.1 on 24.11 but 4.0.2 on 25.05:
-            fish
           ];
           shellHook = ''
             echo "Welcome to the Nix-based development environment shell! It has all required tools."
-
-            # NB: We CANNOT "exec fish" here, because this breaks `nix develop --command`.
-            # TODO How to enter the user's preferred $SHELL in nix develop, instead of hard-code my fish preference?
-            # exec fish
           '';
         };
       });
