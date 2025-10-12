7d6819e4bef64352c34669e0ae2a4b2e4868473a
Michael Vorburger <mike@vorburger.ch>
2025-10-12 20:45:41 +0200

    nix: Test 3rd "hello" mach

--- nix/os/tests/README.md
+++ nix/os/tests/README.md
@@ -2,10 +2,6 @@
 
 Nix allows extremely fast (~8s!) VM integration tests:
 
-    nix build -L
-
-or:
-
     nix flake check
 
 [Testing Notes](../../docs/testing.md) has more.
--- nix/os/tests/demo.nix
+++ nix/os/tests/demo.nix
@@ -1,20 +1,24 @@
 # https://nixcademy.com/posts/nixos-integration-tests-part-2/
 {
-  name = "Two machines ping each other";
+  name = "Two machines ping each other; another one has hello";
 
   nodes = {
     # These configs do not add anything to the default system setup
     machine1 = { pkgs, ... }: { };
     machine2 = { pkgs, ... }: { };
+    machine3 = import ./hello.nix;
   };
 
   testScript = ''
     start_all()
-    for m in [machine1, machine2]:
+    for m in [machine1, machine2, machine3]:
       m.systemctl("start network-online.target")
       m.wait_for_unit("network-online.target")
 
     machine1.succeed("ping -c 1 machine2")
     machine2.succeed("ping -c 1 machine1")
+
+    machine3.succeed("hello")
+    machine2.fail("hello")
   '';
 }
