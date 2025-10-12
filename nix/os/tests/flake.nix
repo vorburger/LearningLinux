4e777728d1c2a82dc9a3a7cd719c28c27bddd3f9
Michael Vorburger <mike@vorburger.ch>
2025-10-12 20:27:53 +0200

    nix: Simplify runNixOSTest to only be a check, no need for a package, actually!

--- nix/os/tests/flake.nix
+++ nix/os/tests/flake.nix
@@ -3,8 +3,6 @@
   outputs =
     { self, nixpkgs }:
     {
-      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.testers.runNixOSTest ./demo.nix;
-
-      checks.x86_64-linux.default = self.packages.x86_64-linux.default;
+      checks.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.testers.runNixOSTest ./demo.nix;
     };
 }
